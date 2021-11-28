9098d4bc3c1c94bf1aec26e24579a4f304880e2c
whk06061 <63198157+whk06061@users.noreply.github.com>
2021-11-29 04:35:28 +0900

    firebase 각 채팅방에 채팅 데이터 저장

--- app/src/main/java/com/yum/chatting_project/MainActivity.kt
+++ app/src/main/java/com/yum/chatting_project/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
@@ -24,18 +26,22 @@ class MainActivity : AppCompatActivity() {
 
     //이메일 로그인 작업위한 변수
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //채팅방 키 값 받아옴
+        val intent = getIntent()
+        val chatroomkey = intent.getStringExtra("채팅방키");
+        //Log.d("키값", data!!)
         auth = Firebase.auth
 
         val email = auth.currentUser!!.email.toString()
         //파이어베이스에 데이터 쓰기위한 변수
-        val database = Firebase.database
-        val myRef = database.getReference("chatting")
+        //val database = Firebase.database
+        //val myRef = database.getReference("chatting")
 
         //리사이클러뷰에 들어갈 아이템 추가
         val items = mutableListOf<ChatData>()
@@ -46,7 +52,56 @@ class MainActivity : AppCompatActivity() {
         rv.adapter = rvAdapter
         rv.layoutManager = LinearLayoutManager(this)
 
+        //채팅방 데이터베이스 하위에 채팅 데이터 저장하기
+        database = Firebase.database.reference
+
         //파이어베이스에 데이터가 업데이트 될때마다 실행
+        database.child("chatrooms").child(chatroomkey!!)
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    items.clear()
+                    for (dataModel in snapshot.children) {
+                        //Log.d("형검사", dataModel.value!!.javaClass.name.toString())
+
+                        //채팅방 데이터베이스에서 채팅데이터 찾기
+                        if (dataModel.value!!.javaClass.name == "java.util.HashMap") {
+                            // Log.d("데이터", dataModel.toString())
+                            val saved_email = dataModel.getValue<ChatData>()!!.nickname
+                            //내 닉네임은 "나", 상대방 닉네임은 이메일
+                            if (saved_email == email) {
+                                items.add(
+                                    ChatData(
+                                        "나",
+                                        dataModel.getValue<ChatData>()!!.msg,
+                                        saved_email
+                                    )
+                                )
+                            } else {
+                                items.add(
+                                    ChatData(
+                                        dataModel.getValue<ChatData>()!!.email,
+                                        dataModel.getValue<ChatData>()!!.msg,
+                                        saved_email
+                                    )
+                                )
+                            }
+                        }
+                        // 채팅방 데이터베이스에서 title 값 찾기
+                        else if (dataModel.value!!.javaClass.name == "java.lang.String") {
+                            val rv_chatroomtitle = findViewById<TextView>(R.id.tv_chatroom_title)
+                            rv_chatroomtitle.setText(dataModel.getValue<String>())
+                        }
+                    }
+                    //items에 변화가 생기면 반영
+                    rvAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        /*
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
                 items.clear()
@@ -74,7 +129,7 @@ class MainActivity : AppCompatActivity() {
                 TODO("Not yet implemented")
             }
 
-        })
+        })*/
 
         //Log.d("회원정보", auth.currentUser!!.uid)
 
@@ -84,14 +139,12 @@ class MainActivity : AppCompatActivity() {
         //        items.add(ChatData("냥냥이","저두요"))
 
 
-
-
-
         val sendBtn = findViewById<Button>(R.id.btn_chat_send)
         sendBtn.setOnClickListener {
             val message = findViewById<EditText>(R.id.editText_chat_msg)
-            val chatData = ChatData(email, message.text.toString(),email)
-            myRef.push().setValue(chatData)
+            val chatData = ChatData(email, message.text.toString(), email)
+            //myRef.push().setValue(chatData)
+            database.child("chatrooms").child(chatroomkey!!).push().setValue(chatData)
             message.setText("")
 
             //키보드 내리기

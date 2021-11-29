76bede87b2f631fab2a48a6217494e5c86d748bc
whk06061 <63198157+whk06061@users.noreply.github.com>
2021-11-30 02:29:09 +0900

    User db 따로 만들어서 회원가입시 회원정보 저장

--- app/src/main/java/com/yum/chatting_project/LoginActivity.kt
+++ app/src/main/java/com/yum/chatting_project/LoginActivity.kt
@@ -9,19 +9,21 @@ import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = Firebase.auth
-
-
+        database = Firebase.database.reference
 
         val loginBtn = findViewById<Button>(R.id.btn_login).setOnClickListener {
 
@@ -46,16 +48,20 @@ class LoginActivity : AppCompatActivity() {
 
         val joinBtn = findViewById<Button>(R.id.btn_join).setOnClickListener {
             val email = findViewById<EditText>(R.id.editText_login_email)
+            val email_text = email.text.toString()
             val password = findViewById<EditText>(R.id.editText_login_password)
+            val password_text = password.text.toString()
 
             //회원가입
-            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            auth.createUserWithEmailAndPassword(email_text, password_text)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
                         Toast.makeText(this,"회원가입 성공!",Toast.LENGTH_LONG).show()
                         email.setText("")
                         password.setText("")
+                        val userData = UserData(email_text,email_text)
+                        database.child("Users").child(auth.currentUser!!.uid).setValue(userData)
 
                     } else {
                         // If sign in fails, display a message to the user.

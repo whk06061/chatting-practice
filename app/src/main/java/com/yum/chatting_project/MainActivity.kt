11ab948f62d679dfbff50e2906b417e8fa76fd99
whk06061 <63198157+whk06061@users.noreply.github.com>
2021-11-30 02:30:57 +0900

    ValueEventListener -> ChildEventListener로 변경 / 나가기 버튼 누를 시 채팅방 참여자 정보 false로 설정

--- app/src/main/java/com/yum/chatting_project/MainActivity.kt
+++ app/src/main/java/com/yum/chatting_project/MainActivity.kt
@@ -14,10 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
@@ -32,83 +29,182 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_include_drawer)
 
-        //버튼을 눌러 메뉴를 오픈할 수도 있고, 왼쪽에서 오른쪽으로 스왑해 오픈할 수 있습니다.
-        //DrawerLayout의 id에 직접 openDrawer()메소드를 사용할 수 있습니다.
-        findViewById<Button>(R.id.menu_btn).setOnClickListener{
-            findViewById<DrawerLayout>(R.id.main_drawer_layout).openDrawer(GravityCompat.END)
-        }
+
+        //채팅방 데이터베이스 하위에 채팅 데이터 저장하기
+        database = Firebase.database.reference
+
+        auth = Firebase.auth
+
+        val current_email = auth.currentUser!!.email.toString()
+        val current_uid = auth.currentUser!!.uid.toString()
 
         //채팅방 키 값 받아옴
         val intent = getIntent()
         val chatroomkey = intent.getStringExtra("채팅방키");
         //Log.d("키값", data!!)
-        auth = Firebase.auth
 
-        val email = auth.currentUser!!.email.toString()
-        //파이어베이스에 데이터 쓰기위한 변수
-        //val database = Firebase.database
-        //val myRef = database.getReference("chatting")
+        //버튼을 눌러 메뉴를 오픈할 수도 있고, 왼쪽에서 오른쪽으로 스왑해 오픈할 수 있습니다.
+        //DrawerLayout의 id에 직접 openDrawer()메소드를 사용할 수 있습니다.
+        findViewById<Button>(R.id.menu_btn).setOnClickListener {
+            findViewById<DrawerLayout>(R.id.main_drawer_layout).openDrawer(GravityCompat.END)
+        }
+
+        //채팅방 나가기
+        findViewById<Button>(R.id.btn_exit_room).setOnClickListener {
+            //사용자 값 false로
+            database.child("ChatRooms").child(chatroomkey!!).child("users").child(current_uid).setValue(false)
+        }
+
+
 
         //리사이클러뷰에 들어갈 아이템 추가
-        val items = mutableListOf<ChatData>()
+        val chats = mutableListOf<ChatData>()
+        val roomusers = mutableListOf<String>()
 
         //리사이클러뷰 어댑터 연결
         val rv = findViewById<RecyclerView>(R.id.recycler_view)
-        val rvAdapter = Chat_RVAdapter(items, this)
+        val rvAdapter = Chat_RVAdapter(chats, this)
         rv.adapter = rvAdapter
         rv.layoutManager = LinearLayoutManager(this)
 
-        //채팅방 데이터베이스 하위에 채팅 데이터 저장하기
-        database = Firebase.database.reference
 
-        //파이어베이스에 데이터가 업데이트 될때마다 실행
-        database.child("chatrooms").child(chatroomkey!!)
-            .addValueEventListener(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    items.clear()
-                    for (dataModel in snapshot.children) {
-                        //Log.d("형검사", dataModel.value!!.javaClass.name.toString())
+        val mChildEventListener = object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+               // Log.d("데이터", "추가됨")
+                val saved_email = snapshot.getValue<ChatData>()!!.email
+                val saved_uid = snapshot.getValue<ChatData>()!!.uid
+                //내 닉네임은 "나", 상대방 닉네임은 이메일
+                if (saved_uid == current_uid) {
+                    chats.add(
+                        ChatData(
+                            "나",
+                            snapshot.getValue<ChatData>()!!.msg,
+                            saved_email, saved_uid
+                        )
+                    )
+                    //Log.d("데이터", items.toString())
+                } else {
+                    chats.add(
+                        ChatData(
+                            saved_email,
+                            snapshot.getValue<ChatData>()!!.msg,
+                            saved_email, saved_uid
+                        )
+                    )
+                }
+                //items에 변화가 생기면 반영
+                rvAdapter.notifyDataSetChanged()
+            }
 
-                        //채팅방 데이터베이스에서 채팅데이터 찾기
-                        if (dataModel.value!!.javaClass.name == "java.util.HashMap") {
-                            // Log.d("데이터", dataModel.toString())
-                            val saved_email = dataModel.getValue<ChatData>()!!.nickname
-                            //내 닉네임은 "나", 상대방 닉네임은 이메일
-                            if (saved_email == email) {
-                                items.add(
-                                    ChatData(
-                                        "나",
-                                        dataModel.getValue<ChatData>()!!.msg,
-                                        saved_email
-                                    )
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        val mChildEventListener2 = object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("추가됨", snapshot.toString())
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("업데이트됨", snapshot.toString())
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        /*val mValueEventListener1 = object : ValueEventListener {
+            //나중에 과부하 줄이려면 ChildEventListener 사용하고(추가된 데이터만 반환함) 리사이클러뷰에 그 아이템만 추가하는 방식 써야될듯
+            override fun onDataChange(snapshot: DataSnapshot) {
+                chats.clear()
+                for (dataModel in snapshot.children) {
+                    //Log.d("형검사", dataModel.value!!.javaClass.name.toString())
+                    Log.d("데이터1", dataModel.toString())
+                    //채팅방 데이터베이스에서 채팅데이터 찾기
+                    if (dataModel.value!!.javaClass.name == "java.util.HashMap") {
+                        //Log.d("데이터2", dataModel.toString())
+                        val saved_email = dataModel.getValue<ChatData>()!!.email
+                        val saved_uid = dataModel.getValue<ChatData>()!!.uid
+                        //내 닉네임은 "나", 상대방 닉네임은 이메일
+                        if (saved_uid == current_uid) {
+                            chats.add(
+                                ChatData(
+                                    "나",
+                                    dataModel.getValue<ChatData>()!!.msg,
+                                    saved_email, saved_uid
                                 )
-                            } else {
-                                items.add(
-                                    ChatData(
-                                        dataModel.getValue<ChatData>()!!.email,
-                                        dataModel.getValue<ChatData>()!!.msg,
-                                        saved_email
-                                    )
+                            )
+                            //Log.d("데이터", items.toString())
+                        } else {
+                            chats.add(
+                                ChatData(
+                                    saved_email,
+                                    dataModel.getValue<ChatData>()!!.msg,
+                                    saved_email, saved_uid
                                 )
-                            }
-                        }
-                        // 채팅방 데이터베이스에서 title 값 찾기
-                        else if (dataModel.value!!.javaClass.name == "java.lang.String") {
-                            val rv_chatroomtitle = findViewById<TextView>(R.id.tv_chatroom_title)
-                            rv_chatroomtitle.setText(dataModel.getValue<String>())
+                            )
                         }
                     }
-                    //items에 변화가 생기면 반영
-                    rvAdapter.notifyDataSetChanged()
+                    // 채팅방 데이터베이스에서 title 값 찾기
+                    else if (dataModel.value!!.javaClass.name == "java.lang.String") {
+                        /*val rv_chatroomtitle = findViewById<TextView>(R.id.tv_chatroom_title)
+                        rv_chatroomtitle.setText(dataModel.getValue<String>())*/
+                    }
                 }
+                //items에 변화가 생기면 반영
+                rvAdapter.notifyDataSetChanged()
+            }
 
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
 
-            })
+        }
+*/
+        val mValueEventListener2 = object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        }
 
+        //파이어베이스에 데이터가 업데이트 될때마다 실행
+        database.child("Messages").child(chatroomkey!!)
+            .addChildEventListener(mChildEventListener)
+
+        database.child("ChatRooms").child(chatroomkey!!).child("users")
+            .addChildEventListener(mChildEventListener2)
+
+        /* database.child("Messages").child(chatroomkey!!)
+             .addValueEventListener(mValueEventListener1)*/
+
+        /*database.child("ChatRooms").child(chatroomkey!!).child("users")
+            .addValueEventListener(mValueEventListener2)*/
         //Log.d("회원정보", auth.currentUser!!.uid)
 
         //        items.add(ChatData("햄찌","안녕하세요~!"))
@@ -120,10 +216,12 @@ class MainActivity : AppCompatActivity() {
         val sendBtn = findViewById<Button>(R.id.btn_chat_send)
         sendBtn.setOnClickListener {
             val message = findViewById<EditText>(R.id.editText_chat_msg)
-            val chatData = ChatData(email, message.text.toString(), email)
+            val chatData =
+                ChatData(current_email, message.text.toString(), current_email, current_uid)
             //myRef.push().setValue(chatData)
-            database.child("chatrooms").child(chatroomkey!!).push().setValue(chatData)
+            database.child("Messages").child(chatroomkey!!).push().setValue(chatData)
             message.setText("")
+
 
             //키보드 내리기
             val manager: InputMethodManager =

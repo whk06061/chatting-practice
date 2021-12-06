400a595210b9afc5517a3c58092d9f57ee479624
whk06061 <63198157+whk06061@users.noreply.github.com>
2021-12-07 02:07:27 +0900

    채팅방 생성하면 회원 모두 참여시킴 & 사용자 참여 채팅방만 보여주게 코드 수정

--- app/src/main/java/com/yum/chatting_project/ChatListActivity.kt
+++ app/src/main/java/com/yum/chatting_project/ChatListActivity.kt
@@ -6,14 +6,14 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
@@ -21,14 +21,19 @@ import com.google.firebase.ktx.Firebase
 class ChatListActivity : AppCompatActivity() {
 
     private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_list)
 
+        auth = Firebase.auth
+        val current_uid = auth.currentUser!!.uid
+
         //리사이클러뷰에 들어갈 아이템 추가
         val items = mutableListOf<ChatListData>()
         val chatroomkeys = mutableListOf<String>()
+        val users = mutableListOf<String>()
 
         //리사이클러뷰 어댑터 연결
         val rv = findViewById<RecyclerView>(R.id.chat_list_rv)
@@ -37,24 +42,67 @@ class ChatListActivity : AppCompatActivity() {
         rv.adapter = rvAdapter
         rv.layoutManager = LinearLayoutManager(this)
         //아이템 사이 구분선 추가
-        rv.addItemDecoration(DividerItemDecoration(this,1))
+        rv.addItemDecoration(DividerItemDecoration(this, 1))
 
         //1. 파이어베이스에서 채팅방 불러오기
         database = Firebase.database.reference
-        database.child("ChatRooms").addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                items.clear()
-                chatroomkeys.clear()
-                for (dataModel in snapshot.children) {
-                    Log.d("아이템1", dataModel.toString())
-                    items.add(dataModel.getValue(ChatListData::class.java)!!)
-                    //채팅방 고유 키 저장
-                    chatroomkeys.add(dataModel.key!!)
+
+        //사용자가 참여한 채팅방만 보여줌
+        database.child("UserRooms").child(current_uid)
+            .addChildEventListener(object : ChildEventListener {
+                override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                    //사용자가 참여한 채팅방을 리사이클러뷰에 추가
+                    database.child("ChatRooms").child(snapshot.value.toString()).get().addOnSuccessListener {
+                        val key = it.key
+                        items.add(it.getValue(ChatListData::class.java)!!)
+                        //채팅방 고유 키 저장
+                        chatroomkeys.add(key!!)
+                        rvAdapter.notifyDataSetChanged()
+                    }
                 }
-                //items에 변화가 생기면 반영
-                rvAdapter.notifyDataSetChanged()
-                //Log.d("키", chatroomkeys.toString())
-                //Log.d("아이템2", items.toString())
+
+                override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                    val key = snapshot.key
+                    Log.d("키다", key.toString())
+                }
+
+                override fun onChildRemoved(snapshot: DataSnapshot) {
+                    val key = snapshot.key
+                    Log.d("키다", key.toString())
+                }
+
+                override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                    val key = snapshot.key
+                    Log.d("키다", key.toString())
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("키다", error.toString())
+                }
+
+            }
+            )
+
+
+        //가입된 회원들 정보 가져오기
+        database.child("Users").addChildEventListener(object : ChildEventListener {
+
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                Log.d("아이템1", snapshot.toString())
+                users.add(snapshot.key.toString())
+
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -81,7 +129,16 @@ class ChatListActivity : AppCompatActivity() {
                 val chatroom_title =
                     mAlertDialog.findViewById<EditText>(R.id.dialog_editText_chatroom_name)
                 val chatListData = ChatListData(chatroom_title!!.text.toString())
-                database.child("ChatRooms").push().setValue(chatListData)
+
+                //임시로!!!!!!! 채팅방 생성하면 모든 회원 참여시킴
+                var chatroomKey = database.child("ChatRooms").push().key
+                database.child("ChatRooms").child(chatroomKey!!).setValue(chatListData)
+                for (user in users) {
+                    database.child("ChatRooms").child(chatroomKey!!).child("users").child(user)
+                        .setValue(true)
+                    //각 사용자가 무슨 채팅방에 참여하고 있는지 저장
+                    database.child("UserRooms").child(user).push().setValue(chatroomKey)
+                }
 
                 //다이얼로그 사라지게
                 mAlertDialog.dismiss()
